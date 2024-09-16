875933989a2f107dfd3c12fbe14606978a8ece2a
soniatl <30045332@cc.peralta.edu>
2024-09-16 02:17:25 -0700

    main.cpp

--- class_activity9/main.cpp
+++ class_activity9/main.cpp
@@ -3,8 +3,7 @@
 //  class_activity9
 //
 //  Created by Sonia Lopchan on 9/15/24.
-//Write a program that checks whether an applicant is eligible for a job.
-//To be eligible, the applicant must be a US citizen and should either have a bachelor's degree or at least two years of experience.
+//
 
 #include <iostream>
 #include <string>
@@ -19,10 +18,24 @@ int main()
     cout << "Are you a US citizen? (Yes/No): ";
     cin >> citizenship;
     
-    if (citizenship!="Yes") {
-        cout << "You're not eligible for a job"<<endl;
+    if (citizenship != "Yes") {
+        cout << "You're not eligible for a job" <<endl;
         return 0;
     }
     
+    cout << "Do you have a bachelor's degree? (Yes/No): ";
+    cin >> degree;
     
+    if (degree != "Yes") {
+        cout << "How many years of experiences do you have? ";
+        cin >> experience;
+        
+        if (experience < 2) {
+            cout << "You need at least two years of experience to qualify for this job"<< endl;
+            return 0;
+        }
+    }
+    cout << "You're eligible to apply for this job" << endl;
+    
+    return 0;
 }
