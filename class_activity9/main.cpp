f22f809d0222c50a798dba77d2f269d718920aed
Sonia <30045332@cc.peralta.edu>
2024-09-15 23:34:58 -0700

    Initial Commit

---
+++ class_activity9/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  class_activity9
+//
+//  Created by Sonia Lopchan on 9/15/24.
+//Write a program that checks whether an applicant is eligible for a job.
+//To be eligible, the applicant must be a US citizen and should either have a bachelor's degree or at least two years of experience.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string
+}
